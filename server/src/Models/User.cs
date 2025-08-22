b7024151827a4ced8af87016594ed2b22b167989
Yash Kiradoo <kiradooyash2924@gmail.com>
2025-08-22 14:21:46 +0000

    Backend Files commit.

---
+++ server/src/Models/User.cs
@@ -0,0 +1,12 @@
+namespace HisabKitab.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<Expense> Expenses { get; set; }
+    }
+}
